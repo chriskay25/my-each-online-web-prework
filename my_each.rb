6f22a52c11164809310a8f5ce8538bc86f22b2b0
Chris Kay <chriskay25@gmail.com>
2019-04-14 16:57:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,10 +3,14 @@ def my_each(array)
   while i < array.length
     yield(array[i])
     i = i + 1
+  end
+  array
 end
 
 collection = [1,2,3,4]
 
-my_each(collection)
+my_each(collection) do |i|
+  puts i 
+end
 
 
